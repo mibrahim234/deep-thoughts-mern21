4625ddb69eabf3e69f889aa5f3ecfcba37448004
mibrahim234 <mohamm1417@gmail.com>
2022-01-11 19:06:39 -0500

    build files

---
+++ client/build/precache-manifest.cc92d4b071f03a7c16f8a8c3b55d81e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0f0fc9f3a8210a828d0169d65faf89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "971d4f07c8a61c15b997",
+    "url": "/static/css/main.505921b4.chunk.css"
+  },
+  {
+    "revision": "e0958737a4b34b1989f5",
+    "url": "/static/js/2.b524cee9.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.b524cee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a1fcf160074d594e9e",
+    "url": "/static/js/3.2830927d.chunk.js"
+  },
+  {
+    "revision": "971d4f07c8a61c15b997",
+    "url": "/static/js/main.b465c1c5.chunk.js"
+  },
+  {
+    "revision": "f11df26bdd987e46be19",
+    "url": "/static/js/runtime-main.d62f40cc.js"
+  }
+]);
